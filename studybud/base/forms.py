ace47b84c9943bff9e287fe87fbce266057b6596
Jeffry Chavenia <41783921+chaveniajeffry@users.noreply.github.com>
2023-02-23 04:22:07 +0800

    feat(studybud): add the whole project

---
+++ studybud/base/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+from .models import Room
+
+
+class RoomForm(ModelForm):
+    class Meta:
+        model = Room
+        # Will have an input in all fields of model
+        fields = '__all__'
