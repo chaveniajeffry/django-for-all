c5f07b9fa66a2a75b351c01e051116b81a54bb30
Jeffry Chavenia <41783921+chaveniajeffry@users.noreply.github.com>
2023-02-24 10:12:22 +0800

    feat(profile): add profile feat and update url

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -9,12 +9,7 @@ from django.contrib.auth.models import User
 from .forms import RoomForm
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-# rooms = [
-#     {'id':1,'name':'Lets learn python'},
-#     {'id':2,'name':'Design with me'},
-#     {'id':3,'name':'Frontend Developers'},
-# ]
+
 
 def loginPage(request):
     page = 'login'
@@ -62,6 +57,7 @@ def registerUser(request):
         'form': form,
     }
     return render(request, 'base/login_register.html',context)
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(
@@ -71,10 +67,14 @@ def home(request):
         )
     room_count = rooms.count()
     topics = Topic.objects.all()
+    room_messages = Message.objects.filter(
+        Q(room__topic__name__icontains=q)
+    )
     context = {
         'room_count'    : room_count,
         'rooms'         : rooms,
         'topics'        : topics,
+        'room_messages' : room_messages,
     }
     return render(request, "base/home.html", context)
 
@@ -98,13 +98,28 @@ def room(request, pk):
     }
     return render(request, "base/room.html", context)
 
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context={
+        'user'          : user,
+        'rooms'         : rooms,
+        'topics'        : topics,
+        'room_messages' : room_messages,
+    }
+    return render(request,'base/profile.html',context)
+
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect('home')
         print(request.POST)
     context = {'form':form}
